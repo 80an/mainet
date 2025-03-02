196f1fe88f513b9a1311c1d1ef7755214e3d23ab
80Humor <102892204+80an@users.noreply.github.com>
2025-03-02 10:50:43 +0300

    Update SyncInfo.sh

--- xpla/SyncInfo.sh
+++ xpla/SyncInfo.sh
@@ -1,5 +1,5 @@
 while true; do 
-  local_height=$(xplad status | jq -r "latest_block_height":); 
+  local_height=$(xplad status | jq -r '.SyncInfo.latest_block_height'); 
   network_height=$(curl -s https://og-testnet-rpc.itrocket.net/status | jq -r 'latest_block_height')
   blocks_left=$((network_height - local_height)); 
   echo -e "\033[1;38mYour node height:\033[0m \033[1;34m$local_height\033[0m | \033[1;35mNetwork height:\033[0m \033[1;36m$network_height\033[0m | \033[1;29mBlocks left:\033[0m \033[1;31m$blocks_left\033[0m"; 
